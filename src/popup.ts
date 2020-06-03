3e89384cc7a289a1e73304e82b71448b51793ea7
moonrailgun <moonrailgun@gmail.com>
2020-06-03 22:09:24 +0800

    add less support

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,5 @@
 import * as $ from 'jquery';
+import './style/popup.less';
 
 const guess = (text: string) => {
   chrome.runtime.sendMessage(
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,44 +1,54 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const srcDir = '../src/';
 
 module.exports = {
-    entry: {
-        popup: path.join(__dirname, srcDir + 'popup.ts'),
-        options: path.join(__dirname, srcDir + 'options.ts'),
-        background: path.join(__dirname, srcDir + 'background.ts'),
-        content_script: path.join(__dirname, srcDir + 'content_script.ts')
+  entry: {
+    popup: path.join(__dirname, srcDir + 'popup.ts'),
+    options: path.join(__dirname, srcDir + 'options.ts'),
+    background: path.join(__dirname, srcDir + 'background.ts'),
+    content_script: path.join(__dirname, srcDir + 'content_script.ts'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
     },
-    output: {
-        path: path.join(__dirname, '../dist/js'),
-        filename: '[name].js'
-    },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    plugins: [
-        // exclude locale files in moment
-        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-        new CopyPlugin([
-            { from: '.', to: '../' }
-          ],
-          {context: 'public' }
-        ),
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.less'],
+  },
+  plugins: [
+    // exclude locale files in moment
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CopyPlugin([{ from: '.', to: '../' }], { context: 'public' }),
+  ],
 };
