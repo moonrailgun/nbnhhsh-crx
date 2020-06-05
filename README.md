0e2a1e785aaeb847bba546108d7578c4095e5cc0
moonrailgun <moonrailgun@gmail.com>
2020-06-05 21:12:02 +0800

    美化页面

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Chrome Extension for nbnhhsh 能不能好好说话Chrome插件版
 
 原版：https://github.com/itorr/nbnhhsh
+
+## Usage
+
+点开拓展弹出框
+
+输入任意想要查询的内容或者在页面上选中任意想要翻译的文本后打开拓展程序
+
+仅支持英文字符输入
--- src/popup.ts
+++ src/popup.ts
@@ -2,40 +2,85 @@ import * as $ from 'jquery';
 import './style/popup.less';
 
 const guess = (text: string) => {
+  if (!text) {
+    return;
+  }
+
+  $('#result').text('');
+  $('.loading').show();
+  $('.tip').hide();
+
   chrome.runtime.sendMessage(
     {
       guess: text,
     },
     (response) => {
-      $('#result').text(JSON.stringify(response));
+      $('.loading').hide();
+
+      if (!Array.isArray(response)) {
+        return;
+      }
+
+      const items = response
+        .filter((item) => Array.isArray(item.trans))
+        .map((item) => {
+          const { name, trans } = item;
+
+          return `<div class="item"><h2>${name}</h2><p>${trans.join(
+            ', '
+          )}</p></div>`;
+        });
+
+      if (items.length > 0) {
+        $('#result').html(items.join(''));
+      } else {
+        $('#result').html('<div class="tip">没有搜索到任意结果</div>');
+      }
     }
   );
-
-  $('#guessText').text(text);
 };
 
 $(function () {
-  const queryInfo = {
-    active: true,
-    currentWindow: true,
-  };
-
   $('#searchBtn').click(() => {
     const val = String($('#input').val());
     guess(val);
   });
 
-  chrome.tabs.query(queryInfo, function (tabs) {
-    chrome.tabs.sendMessage(
-      tabs[0].id,
-      {
-        getText: true,
-      },
-      (response) => {
-        const selectedText = response;
+  $('#input').bind('keydown', (e) => {
+    if (e.which === 13) {
+      const val = String($('#input').val());
+      guess(val);
+    }
+  });
 
-        guess(selectedText);
-      }
+  $('#input').bind('input', () => {
+    // 只允许输入英文
+    // 把所有双字节文本全清空
+    $('#input').val(
+      $('#input')
+        .val()
+        .toString()
+        .replace(/[^\x00-\x80]/gi, '')
     );
   });
+
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    function (tabs) {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          getText: true,
+        },
+        (response) => {
+          const selectedText = response;
+
+          guess(selectedText);
+        }
+      );
+    }
+  );
 });
