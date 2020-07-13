a00e97945016222a5db0777fd03bb1f94337787b
moonrailgun <moonrailgun@gmail.com>
2020-07-13 10:55:40 +0800

    add request cache

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,7 @@
 import * as $ from 'jquery';
 
 const API_URL = 'https://lab.magiconch.com/api/nbnhhsh/';
+const resultCache = new Map();
 
 chrome.runtime.onMessage.addListener(function (request, sender, callback) {
   const { guess } = request;
@@ -8,8 +9,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, callback) {
   if (guess) {
     const text = guess.match(/[a-z0-9]+/gi).join(','); // 分离出一段文字中的文本, 用逗号拼接
 
+    if (resultCache.has(text)) {
+      // 如果缓存中有数据
+      callback(resultCache.get(text));
+      return;
+    }
+
     $.post(`${API_URL}guess`, { text }, (data) => {
       callback(data);
+      resultCache.set(text, data);
     });
   }
 
